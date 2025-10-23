ec5d6e6d346466fc573b431d7aba2a17bb64f89a
Safreen2901saf <safreenfathima2901@gmail.com>
2025-10-23 21:54:13 +0530

    Update app.e2e-spec.ts

--- packages/hoppscotch-backend/test/app.e2e-spec.ts
+++ packages/hoppscotch-backend/test/app.e2e-spec.ts
@@ -3,9 +3,6 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -19,6 +16,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .compile();
   });
 });
